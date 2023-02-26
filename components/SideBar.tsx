cb75625869ef724483919b7a43a128926b98decb
vasylmiskiv <vasylmiskiv@gmail.com>
2023-02-27 01:32:39 +0200

    added chat sidebar component with getting collection of messages

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,18 +1,30 @@
 "use client";
+import { collection } from "firebase/firestore";
+import { signOut, useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
 
 import Image from "next/image";
-import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat";
+import ChatRow from "./ChatRow";
 
 function SideBar() {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session && collection(db, "users", session?.user?.email!, "chats")
+  );
 
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
         <div>
           <NewChat />
-          <div></div>
+          <div>
+            {chats?.docs.map((chat) => (
+              <ChatRow key={chat.id} id={chat.id} />
+            ))}
+          </div>
         </div>
       </div>
       {session && (
